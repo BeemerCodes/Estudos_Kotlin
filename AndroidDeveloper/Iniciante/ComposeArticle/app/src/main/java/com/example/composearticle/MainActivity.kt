7b89b1dd73bff5c6117ec13327eb4e999e33c57c
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2023-01-06 12:03:21 -0300

    WIP

--- AndroidDeveloper/Iniciante/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ AndroidDeveloper/Iniciante/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.composearticle
 
+import android.app.Presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +25,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeArticleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                ){
+
                 }
             }
         }
@@ -30,14 +36,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BackgroundImage() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column{
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PresentationText(title: String, fulltext: String) {
+    Box{
+        Text(
+            text = title,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 100.dp, end = 16.dp)
+        )
+        Text(
+            text = fulltext,
+            fontSize = 24.sp,
+            modifier = Modifier
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BackgroundImagePreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        BackgroundImage()
+        PresentationText(title = "Teste de chamado", fulltext = "Bla bla bla bla")
     }
 }
